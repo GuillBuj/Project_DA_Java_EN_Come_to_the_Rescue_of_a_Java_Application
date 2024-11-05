57906fca8e211985506a86d448796bc6e9afed78
GuillBuj <guillaume.bujon@gmail.com>
2024-11-05 13:11:40 +0100

    push de fin de journée

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,12 +2,26 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer){
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+
+
+
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
@@ -31,10 +45,8 @@ public class AnalyticsCounter {
 		
 		reader.close();
 
-		/*System.out.println("number of headaches: " + headacheCount);
-		System.out.println("number of rashes: " + rashCount);
-		System.out.println("number of pupils: " + pupilCount);*/
 		
+		//pour le test
 		TreeMap<String,Integer> symptoms = new TreeMap<String, Integer>();
 
 		symptoms.put("Headaches", 8);
@@ -43,11 +55,14 @@ public class AnalyticsCounter {
 
 		WriteSymptomDataToFile writeSymptomDataToFile = new WriteSymptomDataToFile();
 		writeSymptomDataToFile.writeSymptoms(symptoms);
-		// next generate output
-		/*FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();*/
 	}
+
+	public List<String> getSymptoms(){
+		return this.reader.GetSymptoms();
+	}
+
+	public Map<String,Integer> sortSymptoms(Map<String,Integer symptomesNonTries){
+		return new TreeMap<>()
+	}
+
 }
